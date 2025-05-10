c3a2973e2da7fe7a8b5a691e7691a899dbd6fb53
bimurto <shawrup.k.suter@gmail.com>
2025-05-10 22:31:57 +0200

    NOREF added

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,32 @@
 plugins {
     id("java")
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("maven-publish")
 }
 
-group = "io.bimurto"
-version = "1.0-SNAPSHOT"
+group = "io.github.bimurto"
+version = "1.0.0"
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            artifactId = "groovy-shell-server"
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/bimurto/groovy-shell-server")
+
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
+
 
 repositories {
     mavenCentral()
@@ -14,7 +36,7 @@ dependencies {
     implementation("org.codehaus.groovy:groovy:3.0.24")
     implementation("org.codehaus.groovy:groovy-groovysh:3.0.24")
     compileOnly("org.springframework:spring-context:5.3.39")
-    implementation("org.apache.sshd:sshd-core:2.15.0"){
+    implementation("org.apache.sshd:sshd-core:2.15.0") {
         exclude(group = "org.slf4j", module = "slf4j-api")
         exclude(group = "org.slf4j", module = "jcl-over-slf4j")
     }
